9bf128a1ba53f024479b16739b50199ea790b6de
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-03-21 13:14:07 +0200

    Add bullet gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ end
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
+  gem 'bullet', '~> 6.1', '>= 6.1.4'
   gem 'web-console', '>= 3.3.0'
 end
 
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.feature 'Users', type: :feature do
   before :each do
-    @usr1 = User.create(username: 'deep', fullname: 'Deepesh Kumar')
-    @usr2 = User.create(username: 'twister', fullname: 'Wingsform')
+    @usr1 = User.create(username: 'tshepo', fullname: 'Tshepo Molefe')
+    @usr2 = User.create(username: 'dav', fullname: 'David')
     visit '/sign_in'
-    fill_in 'session_username', with: 'deep'
+    fill_in 'session_username', with: 'tshepo'
     click_button 'Login'
   end
 
   it 'visit own profile page' do
     visit profile_path(@usr1.username)
-    expect(page).to have_text('ALL BOOKS SUGGESTED BY DEEPESH KUMAR')
+    expect(page).to have_text('ALL FOOTBALL GAME SUGGESTED BY TSHEPO MOLEFE')
   end
 
   it 'visit other profile page' do
-    visit 'users/twister'
-    expect(page).to have_text('ALL BOOKS SUGGESTED BY WINGSFORM')
+    visit 'users/dav'
+    expect(page).to have_text('ALL FOOTBALL GAME SUGGESTED BY DAVID')
   end
 
   it 'follow a user which has profile page open' do
-    visit 'users/twister'
+    visit 'users/dav'
     expect(page).to have_text('0 Following')
     find('a.follow_me').click
     expect(page).to have_text('1 Following')

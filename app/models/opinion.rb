f3fe75b43d0b339fe477771f41e7383c09b359c6
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-05-03 14:00:32 +0200

    Add validation tests for opinion model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -4,5 +4,4 @@ class Opinion < ApplicationRecord
 
   validates :text, presence: true, length: { minimum: 4, maximum: 300 }
   scope :order_by_most_recent, -> { includes(:author).order(created_at: :desc) }
-  
 end
